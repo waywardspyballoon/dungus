efad5b66f84b230f210ce6298bd03a54adee9e71
matt swanson <mattleviswanson@gmail.com>
2023-04-30 22:17:02 -0500

    added .env

--- server.py
+++ server.py
@@ -1,7 +1,25 @@
-from flask import Flask
+import config
+import os
+from flask import Flask, request
+import requests
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>I'm a stupid robot!!1!</p>"
+
+@app.post("/api/bot")
+def bot():
+    data = request.get_json()
+    if data["sender_type"] != "user":
+        return 
+
+    r = requests.post(
+        "https://api.groupme.com/v3/bots/post",
+        json={"bot_id": os.environ["GROUPME_BOT_ID"], "text": "hi"},
+    )
+    return { "data" : "ok"}
+
+
